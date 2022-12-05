196c0907e067cde9c0aa98fc3f8e9decfc2ba5fe
s-unger <github_com@planetcat.de>
2022-12-05 15:12:02 +0100

    Project Setup

---
+++ app/src/main/java/de/planetcat/detecta/DetectAService.kt
@@ -0,0 +1,14 @@
+package de.planetcat.detecta
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class DetectAService : AccessibilityService() {
+    override fun onInterrupt() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
+        TODO("Not yet implemented")
+    }
+}
