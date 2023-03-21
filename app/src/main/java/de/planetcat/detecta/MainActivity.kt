96e1c4ce87b0545fda284736ac67d3369c1c7dd8
s-unger <github_com@planetcat.de>
2023-03-21 11:09:17 +0100

    AccessibilityServices AutoActivate

--- app/src/main/java/de/planetcat/detecta/MainActivity.kt
+++ app/src/main/java/de/planetcat/detecta/MainActivity.kt
@@ -1,13 +1,54 @@
 package de.planetcat.detecta
 
-import androidx.appcompat.app.AppCompatActivity
+import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.AccessibilityServiceInfo
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ServiceInfo
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
+import android.view.accessibility.AccessibilityManager
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.w("DetectAService", "Test: Activity Created")
+
+        findViewById<Button>(R.id.serviceaktivieren)
+            .setOnClickListener {
+                val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+                startActivity(intent)
+            }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (isAccessibilityServiceEnabled(this, DetectAService::class.java)) {
+            findViewById<Button>(R.id.serviceaktivieren).isEnabled = false
+            findViewById<Button>(R.id.serviceaktivieren).text = "Bereits aktiviert."
+        }
+    }
+
+    fun isAccessibilityServiceEnabled(
+        context: Context,
+        service: Class<out AccessibilityService?>
+    ): Boolean {
+        val am: AccessibilityManager =
+            context.getSystemService(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager
+        val enabledServices: List<AccessibilityServiceInfo> =
+            am.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK)
+        for (enabledService in enabledServices) {
+            val enabledServiceInfo: ServiceInfo = enabledService.resolveInfo.serviceInfo
+            if (enabledServiceInfo.packageName.equals(context.getPackageName()) && enabledServiceInfo.name.equals(
+                    service.name
+                )
+            ) return true
+        }
+        return false
     }
 }
