edd9ce7813e2912b6807f5e19c43c27346531584
s-unger <github_com@planetcat.de>
2023-05-08 23:58:34 +0200

    AccessibilityEvent Logging shortening Try

--- app/src/main/java/de/planetcat/detecta/DetectAService.kt
+++ app/src/main/java/de/planetcat/detecta/DetectAService.kt
@@ -1,6 +1,7 @@
 package de.planetcat.detecta
 
 import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.AccessibilityServiceInfo
 import android.app.Activity
 import android.content.Intent
 import android.util.Log
@@ -9,23 +10,34 @@ import android.view.accessibility.AccessibilityEvent
 
 class DetectAService : AccessibilityService() {
 
-    fun getDataObjectId():Int {
-        val sharedPreferences = this.getSharedPreferences("preferences", Activity.MODE_PRIVATE)
-        val dataObjectID = sharedPreferences.getInt("doi", -1)
-        return dataObjectID
-    }
-
-    fun increaseDataObjectId() {
-        val sharedPreferences = this.getSharedPreferences("preferences", Activity.MODE_PRIVATE)
-        with (sharedPreferences.edit()) {
-            putInt("doi", (getDataObjectId()+1))
-            apply()
-        }
-    }
-
     override fun onServiceConnected() {
-        super.onServiceConnected()
+
+        val info = AccessibilityServiceInfo()
+        info.apply {
+            // Set the type of events that this service wants to listen to. Others
+            // won't be passed to this service.
+            eventTypes =
+                AccessibilityEvent.TYPE_VIEW_CLICKED or
+                AccessibilityEvent.TYPE_VIEW_LONG_CLICKED or
+                AccessibilityEvent.TYPE_VIEW_SELECTED or
+                AccessibilityEvent.TYPE_VIEW_FOCUSED or
+                AccessibilityEvent.TYPE_VIEW_FOCUSED or
+                AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED or
+                AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED or
+                AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED or
+                AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED or
+                AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED or
+                AccessibilityEvent.TYPE_ANNOUNCEMENT or
+                AccessibilityEvent.TYPE_GESTURE_DETECTION_START or
+                AccessibilityEvent.TYPE_GESTURE_DETECTION_END or
+                AccessibilityEvent.TYPE_WINDOWS_CHANGED or
+                AccessibilityEvent.TYPE_SPEECH_STATE_CHANGE
+        }
+
+        this.serviceInfo = info
+
     }
+
 
     override fun onUnbind(intent: Intent?): Boolean {
         return super.onUnbind(intent)
@@ -35,9 +47,26 @@ class DetectAService : AccessibilityService() {
         Log.w("DetectAService", "Interrupt Happened")
     }
 
+
+    /*
+    Message-Format:
+    AE [EventType] t[EventTime] p[PackageName] a[Action]*
+    * Only if Action is not zero.
+     */
     override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
+        var message = "AE "
         if (p0 != null) {
-            Logger.log(p0.toString())
+            if (p0.eventType == AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED) {
+                message += (p0.eventType.toString()+" t"+p0.eventTime.toString() + " p"+p0.packageName)
+                if (p0.action != 0) {
+                    message += (" a")
+                }
+
+
+            } else {
+                message += p0.toString()
+            }
         }
+        Logger.log(message)
     }
 }
