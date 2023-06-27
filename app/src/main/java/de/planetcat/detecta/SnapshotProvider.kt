f2b59d3ab163ac2e26bca2eff8d24c3e2dd5530a
s-unger <github_com@planetcat.de>
2023-06-27 05:45:26 +0200

    Remove disturbing Activity_Recognition_Permission

--- app/src/main/java/de/planetcat/detecta/SnapshotProvider.kt
+++ app/src/main/java/de/planetcat/detecta/SnapshotProvider.kt
@@ -3,6 +3,8 @@ package de.planetcat.detecta
 import android.content.Context
 import android.util.Log
 import com.google.android.gms.awareness.Awareness
+import com.google.android.gms.location.ActivityTransition
+import com.google.android.gms.location.DetectedActivity
 
 class SnapshotProvider(private val context: Context, val logger: Logger) {
 
@@ -12,11 +14,12 @@ class SnapshotProvider(private val context: Context, val logger: Logger) {
         Log.w("DetectAService", "Snapshot START")
         Awareness.getSnapshotClient(context).detectedActivity
             .addOnSuccessListener {
-                val newSnapshotInformation = it.toString()
-                Log.w("DetectAService", "SS $newSnapshotInformation")
-                if (newSnapshotInformation != snapshotInformation) {
-                    logger.log("SS $newSnapshotInformation")
-                    snapshotInformation = newSnapshotInformation
+                val activityResult = it.activityRecognitionResult
+                val activity = activityResult.mostProbableActivity
+                val info = "SS " + activity.toString() + " " + activity.confidence
+                if (info != snapshotInformation) {
+                    logger.log("SS $info")
+                    snapshotInformation = info
                     Log.w("DetectAService", "Snapshot LOGGED")
                 }
             }.addOnFailureListener { exception ->
@@ -24,6 +27,24 @@ class SnapshotProvider(private val context: Context, val logger: Logger) {
             }.addOnCanceledListener {
                 Log.w("DetectAService", "Snapshot CANCELED")
             }
+    }
+
+    fun toActivityString(activity: Int): String {
+        return when (activity) {
+            DetectedActivity.STILL -> "STILL"
+            DetectedActivity.WALKING -> "WALKING"
+            DetectedActivity.IN_VEHICLE -> "IN VEHICLE"
+            DetectedActivity.RUNNING -> "RUNNING"
+            else -> "UNKNOWN"
+        }
+    }
+
+    fun toTransitionType(transitionType: Int): String  {
+        return when (transitionType) {
+            ActivityTransition.ACTIVITY_TRANSITION_ENTER -> "ENTER"
+            ActivityTransition.ACTIVITY_TRANSITION_EXIT -> "EXIT"
+            else -> "UNKNOWN"
+        }
     }
 
 }
